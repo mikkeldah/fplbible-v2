afc90e213065f8df994930c3151cc90fbadf6623
Mikkel Dahl Slettebøe <mikkelsletteboe@yahoo.no>
2021-07-25 13:16:34 +0200

    added whitenoise middleware and changed static root in settings.py

--- fantasy-django/settings.py
+++ fantasy-django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0bq)c9(xn-hp9e&f2=h=-cn0ueee1156e_0n$#t^t8uhcic!f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fplbible.herokuapp.com']
+ALLOWED_HOSTS = ['fplbible.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'BASE_DIR' / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
