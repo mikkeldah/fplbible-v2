199984cf8289089c440d5fb7dfe1f4016d25d2ca
Mikkel Dahl Slettebøe <mikkelsletteboe@yahoo.no>
2021-08-15 14:55:08 +0200

    Changes to settings so that users are not able to POST

--- fantasy-django/settings.py
+++ fantasy-django/settings.py
@@ -93,6 +93,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
